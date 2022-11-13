c3eba98c7a8a9f8fa5992b736fd5539e4576273a
Daniele Arminio <87833584+d3xplo1t@users.noreply.github.com>
2022-11-13 23:43:23 +0000

    Create twitterbot.py

---
+++ twitterbot.py
@@ -0,0 +1,32 @@
+import tweepy
+import time
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(
+    "", ""
+)
+auth.set_access_token(
+    "",
+    "",
+)
+# Create API object
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+# Target Hashtags
+KEY = '#python OR #django OR #javascript'    
+
+while True:
+    try:
+        for tweet in tweepy.Cursor(api.search_tweets, q=KEY).items():
+            print('Tweet by @'+ tweet.user.screen_name)
+            tweet.favorite()
+            print('Liked') 
+            time.sleep(200)
+        
+    except tweepy.errors.Forbidden:
+        print("already liked :D")
+        time.sleep(2)  
+    except tweepy.errors.NotFound:
+        print("tweet not found")
+        time.sleep(2)
+
